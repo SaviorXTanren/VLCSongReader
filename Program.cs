8e86337e598c7bd629d216d2c11f7209d6c06600
Matthew Olivo <saviorxtanren@gmail.com>
2022-10-01 12:52:06 -0700

    - Initial check-in

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+while (true)
+{
+    var processes = Process.GetProcessesByName("vlc");
+    if (processes != null && processes.Length > 0)
+    {
+        string songName = processes.First().MainWindowTitle;
+        songName = songName.Replace(" - VLC media player", "");
+
+        string[] splits = songName.Split(new string[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+        if (splits.Length == 2)
+        {
+            File.WriteAllText("Artist.txt", splits[0]);
+            File.WriteAllText("Song.txt", splits[1]);
+        }
+    }
+
+    await Task.Delay(1000);
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VLCSongReader
-Reads the song name and artist from VLC directly via process look-up.
+Reads the song name and artist from VLC directly via process look-up. Requires the song to have Song Name and Artist tags to have VLC display that information properly in the window title.
